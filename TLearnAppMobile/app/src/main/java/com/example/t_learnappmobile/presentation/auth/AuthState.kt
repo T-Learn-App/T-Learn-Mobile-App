1a403ed57067973279a9fac4cc0d94206ecf8a37
Lonnie Huggins <zavarushka7@gmail.com>
2026-02-23 19:05:29 +0500

    fix auth

--- TLearnAppMobile/app/src/main/java/com/example/t_learnappmobile/presentation/auth/AuthState.kt
+++ TLearnAppMobile/app/src/main/java/com/example/t_learnappmobile/presentation/auth/AuthState.kt
@@ -4,6 +4,7 @@ import com.example.t_learnappmobile.data.auth.models.UserData
 
 sealed class AuthState {
     data object Idle : AuthState()
+    
     data object Loading : AuthState()
     data class Success(val user: UserData?) : AuthState()
     data class Error(val messageResId: Int, val args: Array<Any> = emptyArray()) : AuthState()
